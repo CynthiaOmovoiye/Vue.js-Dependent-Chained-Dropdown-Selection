64fc8b77bf86456062628a94ff96e8ba508408e6
shimul <shimul929@gmail.com>
2020-12-05 10:36:27 -0500

    updated index commit

---
+++ files/json.js
@@ -0,0 +1,122 @@
+var category = [
+	{'name':'Laptop','id':0},
+	{'name':'Television','id':1},
+	{'name':'Car','id':2},
+	{'name':'Motorcycle','id':3},
+]
+var subcategory = [
+  {
+    "id": 0,
+    "name": "Apple",
+    "category": 0
+  },
+  {
+    "id": 1,
+    "name": "Acer",
+    "category": 0
+  },
+  {
+    "id": 2,
+    "name": "Asus",
+    "category": 0
+  },
+  {
+    "id": 3,
+    "name": "Dell",
+    "category": 0
+  },
+  {
+    "id": 4,
+    "name": "HP Laptop",
+    "category": 0
+  },
+  {
+    "id": 6,
+    "name": "LG TV",
+    "category": 1
+  },
+  {
+    "id": 7,
+    "name": "Toshiba",
+    "category": 1
+  },
+  {
+    "id": 8,
+    "name": "Samsung TV",
+    "category": 1
+  },
+  {
+    "id": 9,
+    "name": "Sony",
+    "category": 1
+  },
+  {
+    "id": 8,
+    "name": "BMW",
+    "category": 2
+  },
+  {
+    "id": 9,
+    "name": "Toyota",
+    "category": 2
+  },
+  {
+    "id": 9,
+    "name": "Skoda",
+    "category": 2
+  },
+  {
+    "id": 9,
+    "name": "Yamaha",
+    "category": 3
+  },
+  {
+    "id": 9,
+    "name": "Honda",
+    "category": 3
+  }
+]
+
+var products = [
+  {
+    "id": 0,
+    "name": "Apple MacBook Pro $1200.00",
+    "subcategory": 0
+  },
+  {
+    "id": 2,
+    "name": "Apple MacBook Air (M1, 2020) $999.00",
+    "subcategory": 0
+  },
+  {
+    "id": 3,
+    "name": "Acer Chromebook Spin 713 $839.00",
+    "subcategory": 1
+  },
+  {
+    "id": 4,
+    "name": "Asus ZenBook 13 UX325EA $970",
+    "subcategory": 2
+  },
+  {
+    "id": 5,
+    "name": "Dell XPS 13 2-in-1 $1499",
+    "subcategory": 3
+  },
+  {
+    "id": 6,
+    "name": "Dell Zephyrus G14",
+    "subcategory": 3
+  },
+  {
+    "id": 1,
+    "name": "HP Envy x360 13 (2020)",
+    "subcategory": 4
+  },
+  {
+    "id": 8,
+    "name": "HP Envy x360 13 (2020)",
+    "subcategory": 4
+  }
+]
+
